8b8fce73693c9ea292cbf11cf61bd3d4ebfb92fd
argenestel <ag841836@student.nitw.ac.in>
2021-04-22 01:17:59 +0530

    update

---
+++ relenghome/airootfs/root/customize_airootfs.sh
@@ -0,0 +1,23 @@
+#!/usr/bin/env bash
+#
+# SPDX-License-Identifier: GPL-3.0-or-later
+
+set -e -u
+
+# Warning: customize_airootfs.sh is deprecated! Support for it will be removed in a future archiso version.
+
+sed -i 's/#\(en_US\.UTF-8\)/\1/' /etc/locale.gen
+locale-gen
+
+sed -i "s/#Server/Server/g" /etc/pacman.d/mirrorlist
+
+systemctl enable NetworkManager
+systemctl set-default graphical.target
+systemctl enable gdm
+systemctl enable vmtoolsd.service
+systemctl enable vboxservice.service
+systemctl enable bluetooth
+mv /root/lsb-release /etc/lsb-release
+ln -s /usr/bin/calamares /usr/bin/install_cynux
+pacman-key --init
+pacman-key --populate archlinux
